f328a3bc00bb89198b0aa67f747b57d725c961ec
Caio Macedo <caiomacedo0102@gmail.com>
2023-01-15 18:20:40 -0300

    Initial Commit

---
+++ src/main/java/br/com/study/organizationmanager/OrganizationManagementSystemBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.study.organizationmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizationManagementSystemBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrganizationManagementSystemBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/study/organizationmanager/OrganizationManagementSystemBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.study.organizationmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizationManagementSystemBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
